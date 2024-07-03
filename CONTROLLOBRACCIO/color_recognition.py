7754418af6853861918c0d8af590ac109911dffe
Ilmarchi <149374844+Ilmarchi@users.noreply.github.com>
2024-07-03 16:14:50 +0200

    Changed the code so it works with the other arm

--- CONTROLLOBRACCIO/color_recognition.py
+++ CONTROLLOBRACCIO/color_recognition.py
@@ -1,48 +1,24 @@
+#Qui vengono importati tutti i vari moduli che fanno funzionare il programma
+
 import enum
-#ciao
+
 import cv2
-
-def bgr8_to_jpeg(value, quality=75):
-
-     return bytes(cv2.imencode('.jpg', value)[1])
-
-import traitlets
-
-import ipywidgets.widgets as widgets
 
 import time
 
-# Thread function operation library
+import numpy as np
 
-import threading
-
-import inspect
-
-import ctypes
-
-
-origin_widget = widgets.Image(format='jpeg', width=320, height=240)
-
-mask_widget = widgets.Image(format='jpeg',width=320, height=240)
-
-result_widget = widgets.Image(format='jpeg',width=320, height=240)
-
-
-# Create a horizontal box container to place image widgets next to each other
-
-image_container = widgets.HBox([origin_widget, mask_widget, result_widget])
-
-# image_container = widgets.Image(format='jpeg', width=600, height=500)
-
+from Arm_Lib import Arm_Device
 
 
 def get_color(img):
 
      H = []
-
+     S = []
+     V = []
      color_name={}
 
-     img = cv2.resize(img, (640, 480), )
+     img = cv2.resize(img, (640, 480))
 
      # Convert color image to HSV
 
@@ -56,17 +32,19 @@ def get_color(img):
 
      for i in range(280, 360):
 
-         for j in range(180, 260): H.append(HSV[j, i][0])
-
+         for j in range(180, 260): 
+             H.append(HSV[j, i][0])
+             S.append(HSV[j, i][1])
+             V.append(HSV[j, i][2])
      # Calculate the maximum and minimum values of H, S, and V respectively
-
+     
      H_min = min(H);H_max = max(H)
-
-# print(H_min,H_max)
+     S_min = min(S);S_max = max(S)
+     V_min = min(V);V_max = max(V)
 
      # Determine color
 
-     if H_min >= 0 and H_max <= 10 or H_min >= 156 and H_max <= 180: color_name['name'] = 'red'
+     if (( H_min >= 0 and H_max <= 10 ) or ( H_min >= 0 and H_max <= 180 ) ) and ( S_min >= 155 and S_min <= 230 ) and ( V_min >= 160 and V_max <= 220 )  : color_name['name'] = 'red'
 
      elif H_min >= 20 and H_max <= 30: color_name['name'] = 'yellow'
 
@@ -75,20 +53,12 @@ def get_color(img):
      elif H_min >= 100 and H_max <= 112: color_name['name'] = 'blue'
 
      return img, color_name
-
-
-import cv2
-
-import numpy as np
-
-import ipywidgets.widgets as widgets
-
-import time
-from Arm_Lib import Arm_Device
-
+     
+#Initializing the Arm and the VideoCapture (for the pictures)     
+     
 Arm = Arm_Device()
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 
 cap.set(3, 640)
 
@@ -109,134 +79,129 @@ color_upper = np.array([10, 255, 255])
 
 
 
+# Principal function where i take a picture and pass it to a function that returns the color
+
 def Color_Recongnize():
 
     
-    Arm.Arm_serial_servo_write6(s1=90, s2=90, s3=10, s4=0, s5=90, s6=90,time=1000 )
+    Arm.Arm_serial_servo_write6(s1=97, s2=75, s3=22, s4=3, s5=90, s6=90,time=1000 )
     time.sleep(1)
     flag = 0
-    
+    global cap
     while(1):
         # get a frame and show Get the video frame and convert it to HSV format. Use cvtColor() to convert the BGR format to HSV format. The parameter is cv2.COLOR_BGR2HSV.
-
+        flag = 0
         ret, frame = cap.read()
-
+        if not ret:
+            continue
         frame, color_name = get_color(frame)
-
+        
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
         if len(color_name)==1:
-
             global color_lower
 
             global color_upper
-
-
+            # I control the color_name and based on the id i move the arm in the direction
             if color_name['name'] == 'yellow':
                 print( "yellow")
-                moveArm( 1 )
+                flag = 1
                 color_lower = np.array([26, 43, 46])
-
                 color_upper = np.array([34, 255, 255])
-
-                
-
             elif color_name['name'] == 'red':
                 print( "red")
-                #moveArm( 2 )
+                flag = 2
                 color_lower = np.array([0, 43, 46])
-
                 color_upper = np.array([10, 255, 255])
-
-            
-
             elif color_name['name'] == 'green':
                 print( "green")
-                moveArm( 3 )
+                flag = 3
                 color_lower = np.array([35, 43, 46])
-
                 color_upper = np.array([77, 255, 255])
-
-            
-
             elif color_name['name'] == 'blue':
                 print( "blue")
-                moveArm( 4 )
+                flag = 4
                 color_lower=np.array([100, 43, 46])
-
                 color_upper = np.array([124, 255, 255])
-
-
-        origin_widget.value = bgr8_to_jpeg(frame)
-
-        cv2.imshow('Capture', frame)
-
-
-        # change to hsv model
-
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-
-        # get mask Use the inRange() function and the upper and lower bounds of the blue range in the HSV model to obtain the mask. The blue part of the original video in the mask will be made white and the other parts black.
-
-        mask = cv2.inRange(hsv, color_lower, color_upper)
-
-        cv2.imshow('Mask', mask)
-
-        mask_widget.value = bgr8_to_jpeg(mask)
-
-
-
-        # detect blue Perform a bitwise AND operation on the mask on the original video frame, then the white in the mask will be replaced with the real image:
-
-        res = cv2.bitwise_and(frame, frame, mask=mask)
-
-        cv2.imshow('Result', res)
-        cv2.waitKey(10)
-
-        result_widget.value = bgr8_to_jpeg(res)
-
-
+        
+        if not flag == 0 :
+            moveArm( flag )
         time.sleep(0.01)
-            
-            
-
     cap.release()
+        
 
-     #cv2.destroyAllWindows()
+# Function to move the arm in the right direction of the ID
 
 def moveArm( flag ) :
-    Arm.Arm_serial_servo_write6(s1=90, s2=80, s3=10, s4=20, s5=90, s6=90,time=1000 )
+    global cap
+    cap.release()
+    Arm.Arm_serial_servo_write6(s1=95, s2=60, s3=10, s4=45, s5=90, s6=75,time=1000 )
     time.sleep( 2 )
     Arm.Arm_serial_servo_write( 6 , 137,time=1000 )
     time.sleep( 2 )
     if flag == 4 :
-        Arm.Arm_serial_servo_write6(s1=25, s2=90, s3=40, s4=20, s5=75, s6=135,time=1000 )
+        Arm.Arm_serial_servo_write(6,145,100)
+        time.sleep(0.250)
+        Arm.Arm_serial_servo_write6(s1=48, s2=70, s3=40, s4=20, s5=85, s6=145,time=1000 )
         time.sleep(2)
-        Arm.Arm_serial_servo_write6(s1=23, s2=90, s3=0, s4=22, s5=70, s6=135,time=1000 )
+        Arm.Arm_serial_servo_write6(s1=48, s2=55, s3=33, s4=11, s5=86, s6=145,time=1000 )
         time.sleep(2)
         Arm.Arm_serial_servo_write(6,90,500)
-        time.sleep(1)
+        time.sleep(2)
+        Arm.Arm_serial_servo_write6(s1=48, s2=70, s3=40, s4=20, s5=80, s6=145,time=1000 )
+        time.sleep(2)
+        Arm.Arm_serial_servo_write6(s1=97, s2=75, s3=22, s4=3, s5=91, s6=150,time=2000 )
+        time.sleep( 2 )
 
     if flag == 3 :
-        Arm.Arm_serial_servo_write6(s1=157, s2=90, s3=40, s4=20, s5=105, s6=135,time=1000 )
+        Arm.Arm_serial_servo_write(6,145,100)
+        time.sleep(0.250)
+        Arm.Arm_serial_servo_write6(s1=140, s2=70, s3=40, s4=20, s5=105, s6=145,time=1000 )
         time.sleep(2)
-        Arm.Arm_serial_servo_write6(s1=157, s2=90, s3=0, s4=22, s5=110, s6=135,time=1000 )
+        Arm.Arm_serial_servo_write6(s1=140, s2=55, s3=35, s4=9, s5=95, s6=145,time=1000 )
         time.sleep(2)
         Arm.Arm_serial_servo_write(6,90,500)
-        time.sleep(1)
+        time.sleep(2)
+        Arm.Arm_serial_servo_write6(s1=140, s2=70, s3=40, s4=20, s5=105, s6=145,time=1000 )
+        time.sleep(2)
+        Arm.Arm_serial_servo_write6(s1=97, s2=75, s3=22, s4=3, s5=90, s6=150,time=2000 )
+        time.sleep( 2 )
       
     if flag == 1 :
-        Arm.Arm_serial_servo_write6(s1=60, s2=20, s3=90, s4=90, s5=80, s6=135,time= 1000 )
+        Arm.Arm_serial_servo_write(6,145,100)
+        time.sleep(0.250)
+        Arm.Arm_serial_servo_write6(s1=68, s2=30, s3=75, s4=50, s5=100, s6=145,time= 2000 )
         time.sleep(2)
-        Arm.Arm_serial_servo_write6(s1=58, s2=49, s3=45, s4=29, s5=95, s6=135,time=1000 )
+        Arm.Arm_serial_servo_write6(s1=69, s2=18, s3=75, s4=40, s5=100, s6=150,time= 1000 )
         time.sleep(2)
         Arm.Arm_serial_servo_write(6,90,500)
         time.sleep(1)
+        Arm.Arm_serial_servo_write6(s1=68, s2=30, s3=75, s4=50, s5=100, s6=145,time= 2000 )
+        time.sleep(2)
+        Arm.Arm_serial_servo_write6(s1=97, s2=75, s3=22, s4=3, s5=90, s6=150,time=2000 )
+        time.sleep( 2 )
+    if flag == 2 :
+        Arm.Arm_serial_servo_write(6,145,100)
+        time.sleep(0.250)
+        Arm.Arm_serial_servo_write6(s1=125, s2=30, s3=75, s4=47, s5=80, s6=145,time= 2000 )
+        time.sleep(2)
+        Arm.Arm_serial_servo_write6(s1=120, s2=18, s3=75, s4=37, s5=80, s6=150,time= 1000 )
+        time.sleep(2)
+        Arm.Arm_serial_servo_write(6,90,500)
+        time.sleep(1)
+        Arm.Arm_serial_servo_write6(s1=125, s2=30, s3=75, s4=75, s5=80, s6=145,time= 2000 )
+        time.sleep(2)
+        Arm.Arm_serial_servo_write6(s1=97, s2=75, s3=22, s4=3, s5=90, s6=150,time=2000 )
+        time.sleep( 2 )
 
-    Arm.Arm_serial_servo_write6(s1=90, s2=90, s3=10, s4=70, s5=90, s6=90,time=1000 )
-    time.sleep(1)
-    Arm.Arm_serial_servo_write6(s1=90, s2=90, s3=10, s4=0, s5=90, s6=90,time=1000 )
-    
+    # I reinitialize the VideoCapture for the new image
 
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 640)
+    cap.set(4, 480)
+    cap.set(5, 30)  # Set frame rate
+    cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter.fourcc('M', 'J', 'P', 'G'))
 
 try:
     Color_Recongnize()
